34ba03fc47096ca62cc594de84bf4006b887e041
Steven <steven.janssens@oplinter.be>
2015-11-30 19:45:43 +0100

    Update van IOReader en IOWriter (basisversie items toevoegen en refreshen)

--- Opdracht3/src/io/IOReader.java
+++ Opdracht3/src/io/IOReader.java
@@ -1,21 +1,106 @@
 package io;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import domain.Item;
+
 public class IOReader {
 
-	/*
-	 * 1. Creëert een hashmap van items met als key id van item en 
-	 * als value de titel op basis van wat bestaat in de databank.
-	 * 2. Creëert een hashmap van reservaties met als key id van item en als 
-	 * value List van reservatie objecten (cfr. hoe het zit in de boekingenapp)
-	 */
+	private Item item;
 	
-	// Deze methode moet verder afgewerkt worden
-	public Boolean refreshItems() {
-		return true;
+	private HashMap<String,String> movies;
+	private HashMap<String,String> games;
+	private HashMap<String,String> cds;
+	
+	// FILE VERSIE
+	
+	public IOReader() {
+		
 	}
 	
-	// Deze methode moet verder afgewerkt worden
+	public Boolean refreshItems() throws IOException {
+		movies = new HashMap<String, String>();
+        String regel;
+        BufferedReader readerMovies = new BufferedReader(new FileReader("movies.txt"));
+        while ((regel = readerMovies.readLine()) != null)
+        {
+            String[] splits = regel.split("=");
+            String id = splits[0];
+            String titel = splits[1];
+            movies.put(id, titel);
+        }
+        games = new HashMap<String, String>();
+        BufferedReader readerGames = new BufferedReader(new FileReader("games.txt"));
+        while ((regel = readerGames.readLine()) != null)
+        {
+            String[] splits = regel.split("=");
+            String id = splits[0];
+            String titel = splits[1];
+            games.put(id, titel);
+        }
+        cds = new HashMap<String, String>();
+        BufferedReader readerCds = new BufferedReader(new FileReader("cds.txt"));
+        while ((regel = readerCds.readLine()) != null)
+        {
+            String[] splits = regel.split("=");
+            String id = splits[0];
+            String titel = splits[1];
+            cds.put(id, titel);
+        }
+		return true;
+	}
+
 	public Boolean refreshReservaties() {
 		return true;
 	}
+	
+	@SuppressWarnings("resource")
+	public Boolean hasItems(String cat) {
+		BufferedReader input = null;
+		try {
+            String file = cat + ".txt";
+            input = new BufferedReader(new FileReader(file));
+    		if (input.readLine() != null) {
+    		    return true;
+    		}
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+		return false;
+	}
+	
+	public long getAantalItems(String cat) throws IOException {
+		List<String> regels = Files.newBufferedReader(
+								Paths.get(cat + ".txt")).lines().collect(Collectors.toList()
+							  );
+		long aantal = regels.size();
+		return aantal;
+	}
+	
+	public Boolean hasReservaties() {
+		return true;
+	}
+	
+	public HashMap<String,String> getMovies() {
+		return movies;
+	}
+	
+	public HashMap<String,String> getGames() {
+		return games;
+	}
+	
+	public HashMap<String,String> getCDs() {
+		return cds;
+	}
+	
+	public static void main(String [] args) throws IOException {
+	}
+
 }
--- Opdracht3/src/io/IOWriter.java
+++ Opdracht3/src/io/IOWriter.java
@@ -1,13 +1,66 @@
 package io;
 
-public class IOWriter {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
-	public static Boolean schrijfItemWeg() {
-		return false;
+import domain.Item;
+
+public class IOWriter {
+	
+	// FILE VERSIE
+	
+	public IOWriter() {
+		
 	}
 	
-	public static Boolean schrijfReservatieWeg() {
-		return false;
+	public boolean writeItem(Item item) throws IOException {
+		
+		IOReader reader = new IOReader();
+		
+		char type = item.getType();
+		String titel = item.getTitel().replace(' ', '+');
+		
+		String cat = "undefined";
+		if (type == 'm') { cat = "movies"; }
+		else if (type == 'g') { cat = "games"; }
+		else if (type == 'c') { cat = "cds"; }
+		
+		BufferedWriter output = null;
+		
+	    try {
+	    	
+	    	File file = new File(cat + ".txt");
+	        output = new BufferedWriter(new FileWriter(file, true));
+	        long aantalItems = reader.getAantalItems(cat);
+			String id = type + "" + aantalItems;
+			
+			String write = id + "=" + titel;
+	        if (reader.hasItems(cat)) {
+	        	write = "\r\n" + write;
+	        }
+	        output.write(write);
+	        
+	    } catch ( IOException e ) {
+	    	
+	    	e.printStackTrace();
+	    	
+	    } finally {
+	    	
+	    	if ( output != null ) output.close();
+	    	
+	    }
+	    
+	    return true;
+	    
 	}
+	
+	public boolean writeReservatie() {
+		return true;
+	}
+	
+	/*public static void main(String [] args) throws IOException {
+	}*/
 	
 }
