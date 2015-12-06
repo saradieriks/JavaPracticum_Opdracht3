504ad48b19d94abe8c129662ba0eda0f1f57b58d
Kristof <Kristof@Lenovo-PC>
2015-12-06 18:47:13 +0100

    Werkende getPrijs method voor klassen Cd, Film en Spel + Javadoc

--- Opdracht3/src/domain/Cd.java
+++ Opdracht3/src/domain/Cd.java
@@ -2,11 +2,21 @@ package domain;
 
 public class Cd extends Item{
 
+	/**
+	 * Geeft de prijs terug van het artikel. 
+	 * De integer aantalDagen moet het aantal dagen 
+	 * teruggeven dat het artikel is gehuurd.
+	 * <p>
+	 * In dit geval zal de prijs worden berekent op het aantal dagen * huurprijs/dag
+	 * @param	aantalDagen	Het aantal dagen dat het artikel is gehuurd
+	 * @return				De prijs uitgedrukt in een double.
+	 * @see					Item
+	 */
 	
 	public double getPrijs(int aantalDagen){
 		double prijs = 0;
 		
-		
+		prijs += (aantalDagen * 1.5);
 		
 		return prijs;
 	}
--- Opdracht3/src/domain/Film.java
+++ Opdracht3/src/domain/Film.java
@@ -2,12 +2,25 @@ package domain;
 
 public class Film extends Item{
 
+	/**
+	 * Geeft de prijs terug van het artikel. 
+	 * De integer aantalDagen moet het aantal dagen 
+	 * teruggeven dat het artikel is gehuurd.
+	 * <p>
+	 * In dit geval zal de prijs worden berekent op het aantal dagen * huurprijs/dag.
+	 * De eerste 3 dagen zijn gratis.
+	 * @param	aantalDagen	Het aantal dagen dat het artikel is gehuurd
+	 * @return				De prijs uitgedrukt in een double
+	 * @see					Item
+	 */
 	
 	public double getPrijs(int aantalDagen){
-		double prijs = 0;
+		double prijs = 5;
 		
-		//test
-		
+		int dagenOver = aantalDagen - 3;
+		if (dagenOver > 0) {
+			prijs += (dagenOver * 2);
+			
 		return prijs;
 	}
 	
