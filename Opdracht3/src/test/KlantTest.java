28cbabc4460b21db8b69ce892128b683cf2d8b4b
Dries Kesteloot <drieskesteloot@hotmail.com>
2015-12-14 21:36:35 +0100

    Update AdresTest en KlantTest

--- Opdracht3/src/test/KlantTest.java
+++ Opdracht3/src/test/KlantTest.java
@@ -11,53 +11,117 @@ import domain.Klant;
 import domain.Reservatie;
 
 public class KlantTest {
-	private Adres adres;
-	private Klant klant;
+	private Adres adres, adresGelijk, adresNietGelijk;
+	private Klant klant, klantGelijk, klantNietGelijk;
 	
 	@Before
 	public void SetUp()throws Exception {
 		adres = new Adres("Leuvensebaan", "351", "b", "3000", "Leuven", "België");
+		adresGelijk = new Adres("Leuvensebaan", "28", "3000", "Leuven", "België");
+		adresNietGelijk = new Adres("Brusselsebaan", "128", "100", "Brussel", "Frankrijk");
 		klant = new Klant(1, "World", "Hello", adres, "hello.world@world.com");
+		klantGelijk = new Klant(1, "World", "Hello", adres, "hello.world@world.com");
+		klantNietGelijk = new Klant(2, "Sun", "By", adresNietGelijk, "by.sun@sun.com");
 	}
 		
 	@Test
 	public void test_Klant_Met_5_Parameters_Wordt_Aangemaakt() {
 		Klant nieuweKlant = new Klant(1, "World", "Hello", adres, "hello.world@world.com");
+		Adres nieuwAdres = new Adres("Leuvensebaan", "351", "b", "3000", "Leuven", "België");
 		
 		assertEquals(1, nieuweKlant.getKlantID());
 		assertEquals("World", nieuweKlant.getNaam());
 		assertEquals("Hello", nieuweKlant.getVoornaam());
-		assertEquals(new Adres("Brusselsesebaan", "35", "a", "1000", "Brussel", "België"), nieuweKlant.getAdres());
+		assertEquals(nieuwAdres.getStraat(), nieuweKlant.getAdres().getStraat());
+		assertEquals(nieuwAdres.getNummer(), nieuweKlant.getAdres().getNummer());
+		assertEquals(nieuwAdres.getBox(), nieuweKlant.getAdres().getBox());
+		assertEquals(nieuwAdres.getPostcode(), nieuweKlant.getAdres().getPostcode());
+		assertEquals(nieuwAdres.getGemeente(), nieuweKlant.getAdres().getGemeente());
+		assertEquals(nieuwAdres.getLand(), nieuweKlant.getAdres().getLand());
 		assertEquals("hello.world@world.com", nieuweKlant.getEmail());
+		assertEquals(nieuweKlant.getKlantID(), 1);
+		assertEquals(nieuweKlant.getNaam(), "World");
+		assertEquals(nieuweKlant.getVoornaam(), "Hello");
+		assertEquals(nieuweKlant.getAdres().getStraat(), nieuwAdres.getStraat());
+		assertEquals(nieuweKlant.getAdres().getNummer(), nieuwAdres.getNummer());
+		assertEquals(nieuweKlant.getAdres().getBox(), nieuwAdres.getBox());
+		assertEquals(nieuweKlant.getAdres().getPostcode(), nieuwAdres.getPostcode());
+		assertEquals(nieuweKlant.getAdres().getGemeente(), nieuwAdres.getGemeente());
+		assertEquals(nieuweKlant.getAdres().getLand(), nieuwAdres.getLand());
+		assertEquals(nieuweKlant.getEmail(), "hello.world@world.com");
 	}
 	
 	@Test
 	public void test_Klant_Met_4_Parameters_Zonder_KlantID_Wordt_Aangemaakt() {
 		Klant nieuweKlant = new Klant("World", "Hello", adres, "hello.world@world.com");
+		Adres nieuwAdres = new Adres("Leuvensebaan", "351", "b", "3000", "Leuven", "België");
 		
 		assertEquals("World", nieuweKlant.getNaam());
 		assertEquals("Hello", nieuweKlant.getVoornaam());
-		assertEquals(new Adres("Leuvensebaan", "351", "b", "3000", "Leuven", "België"), nieuweKlant.getAdres());
+		assertEquals(nieuwAdres.getStraat(), nieuweKlant.getAdres().getStraat());
+		assertEquals(nieuwAdres.getNummer(), nieuweKlant.getAdres().getNummer());
+		assertEquals(nieuwAdres.getBox(), nieuweKlant.getAdres().getBox());
+		assertEquals(nieuwAdres.getPostcode(), nieuweKlant.getAdres().getPostcode());
+		assertEquals(nieuwAdres.getGemeente(), nieuweKlant.getAdres().getGemeente());
+		assertEquals(nieuwAdres.getLand(), nieuweKlant.getAdres().getLand());
 		assertEquals("hello.world@world.com", nieuweKlant.getEmail());
+		assertEquals(nieuweKlant.getNaam(), "World");
+		assertEquals(nieuweKlant.getVoornaam(), "Hello");
+		assertEquals(nieuweKlant.getAdres().getStraat(), nieuwAdres.getStraat());
+		assertEquals(nieuweKlant.getAdres().getNummer(), nieuwAdres.getNummer());
+		assertEquals(nieuweKlant.getAdres().getBox(), nieuwAdres.getBox());
+		assertEquals(nieuweKlant.getAdres().getPostcode(), nieuwAdres.getPostcode());
+		assertEquals(nieuweKlant.getAdres().getGemeente(), nieuwAdres.getGemeente());
+		assertEquals(nieuweKlant.getAdres().getLand(), nieuwAdres.getLand());
+		assertEquals(nieuweKlant.getEmail(), "hello.world@world.com");
 	}
 	
 	@Test
 	public void test_Klant_Met_4_Parameters_Zonder_Email_Wordt_Aangemaakt() {
 		Klant nieuweKlant = new Klant(1, "World", "Hello", adres);
+		Adres nieuwAdres = new Adres("Leuvensebaan", "351", "b", "3000", "Leuven", "België");
 		
 		assertEquals(1, nieuweKlant.getKlantID());
 		assertEquals("World", nieuweKlant.getNaam());
 		assertEquals("Hello", nieuweKlant.getVoornaam());
-		assertEquals(new Adres("Leuvensebaan", "351", "b", "3000", "Leuven", "België"), nieuweKlant.getAdres());
+		assertEquals(nieuwAdres.getStraat(), nieuweKlant.getAdres().getStraat());
+		assertEquals(nieuwAdres.getNummer(), nieuweKlant.getAdres().getNummer());
+		assertEquals(nieuwAdres.getBox(), nieuweKlant.getAdres().getBox());
+		assertEquals(nieuwAdres.getPostcode(), nieuweKlant.getAdres().getPostcode());
+		assertEquals(nieuwAdres.getGemeente(), nieuweKlant.getAdres().getGemeente());
+		assertEquals(nieuwAdres.getLand(), nieuweKlant.getAdres().getLand());
+		assertEquals(nieuweKlant.getKlantID(), 1);
+		assertEquals(nieuweKlant.getNaam(), "World");
+		assertEquals(nieuweKlant.getVoornaam(), "Hello");
+		assertEquals(nieuweKlant.getAdres().getStraat(), nieuwAdres.getStraat());
+		assertEquals(nieuweKlant.getAdres().getNummer(), nieuwAdres.getNummer());
+		assertEquals(nieuweKlant.getAdres().getBox(), nieuwAdres.getBox());
+		assertEquals(nieuweKlant.getAdres().getPostcode(), nieuwAdres.getPostcode());
+		assertEquals(nieuweKlant.getAdres().getGemeente(), nieuwAdres.getGemeente());
+		assertEquals(nieuweKlant.getAdres().getLand(), nieuwAdres.getLand());
 	}
 	
 	@Test
 	public void test_Klant_Met_3_Parameters_Wordt_Aangemaakt() {
 		Klant nieuweKlant = new Klant("World", "Hello", adres);
+		Adres nieuwAdres = new Adres("Leuvensebaan", "351", "b", "3000", "Leuven", "België");
 		
 		assertEquals("World", nieuweKlant.getNaam());
 		assertEquals("Hello", nieuweKlant.getVoornaam());
-		assertEquals(new Adres("Leuvensebaan", "351", "b", "3000", "Leuven", "België"), nieuweKlant.getAdres());
+		assertEquals(nieuwAdres.getStraat(), nieuweKlant.getAdres().getStraat());
+		assertEquals(nieuwAdres.getNummer(), nieuweKlant.getAdres().getNummer());
+		assertEquals(nieuwAdres.getBox(), nieuweKlant.getAdres().getBox());
+		assertEquals(nieuwAdres.getPostcode(), nieuweKlant.getAdres().getPostcode());
+		assertEquals(nieuwAdres.getGemeente(), nieuweKlant.getAdres().getGemeente());
+		assertEquals(nieuwAdres.getLand(), nieuweKlant.getAdres().getLand());
+		assertEquals(nieuweKlant.getNaam(), "World");
+		assertEquals(nieuweKlant.getVoornaam(), "Hello");
+		assertEquals(nieuweKlant.getAdres().getStraat(), nieuwAdres.getStraat());
+		assertEquals(nieuweKlant.getAdres().getNummer(), nieuwAdres.getNummer());
+		assertEquals(nieuweKlant.getAdres().getBox(), nieuwAdres.getBox());
+		assertEquals(nieuweKlant.getAdres().getPostcode(), nieuwAdres.getPostcode());
+		assertEquals(nieuweKlant.getAdres().getGemeente(), nieuwAdres.getGemeente());
+		assertEquals(nieuweKlant.getAdres().getLand(), nieuwAdres.getLand());
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
@@ -72,6 +136,16 @@ public class KlantTest {
 
 	}
 	
+	@Test
+	public void test_Equals_True_Als_Klanten_Gelijk_Zijn() {
+		//assertTrue(klant.getKlantID
+		assertTrue(klant.getNaam().equals(klantGelijk.getNaam()));
+	}
+	
+	@Test
+	public void test_Equals_False_Als_Klanten_Niet_Gelijk_Zijn() {
+
+	}
 	
 
 }
