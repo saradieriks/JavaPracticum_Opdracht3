3023e72eca7ff48aef60137e9de098a636734165
Dries Kesteloot <drieskesteloot@hotmail.com>
2016-01-08 14:47:35 +0100

    Stateklassen

---
+++ Opdracht3/src/domain/StatusUitgeleend.java
@@ -0,0 +1,26 @@
+package domain;
+
+public class StatusUitgeleend implements Status{
+	Item item;
+	
+	public StatusUitgeleend(Item item){
+		this.item = item;
+	}
+	
+	public void uitlenen(){
+		
+	}
+	
+	public void terugbrengen(){
+		
+	}
+	
+	public void herstellen(){
+		
+	}
+	
+	public void verwijderen(){
+		
+	}
+
+}
---
+++ Opdracht3/src/domain/StatusUitleenbaar.java
@@ -0,0 +1,25 @@
+package domain;
+
+public class StatusUitleenbaar implements Status{
+	Item item;
+	
+	public StatusUitleenbaar(Item item){
+		this.item = item;
+	}
+	
+	public void uitlenen(){
+		
+	}
+	
+	public void terugbrengen(){
+		
+	}
+	
+	public void herstellen(){
+		
+	}
+	
+	public void verwijderen(){
+		
+	}
+}
